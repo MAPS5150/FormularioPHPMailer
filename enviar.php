99daa58f493702bcbfcca940b4122ba500f24cfa
Miguel Perez <dauphin.swim@gmail.com>
2023-11-22 17:33:12 -0600

    Se sube la app del formulario y las bibliotecas de PHPMailer

---
+++ enviar.php
@@ -0,0 +1,41 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    // mandamos llamar a las librerias de phpmailer
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+
+    //Se crea una instancia de la clase PHPMailer; le pasamos el valor de `true` para habilitar exceptions
+    $mail = new PHPMailer(true);
+    try{
+        $emailTo=$_POST["correo"];
+        $subject=$_POST["asunto"];
+        $bodyEmail=$_POST["mensaje"];
+
+        //Ajustes del Servidor para el acceso de envio de correo
+        $mail->SMTPDebug = 0;                       
+        $mail->isSMTP();                                            
+        $mail->Host       = 'mail.subitus.com';                     
+        $mail->SMTPAuth   = true;                                   
+        $mail->Username   = 'miguel.perez@subitus.com';                     
+        $mail->Password   = 'Mexico2022?';                               
+        $mail->SMTPSecure = 'tls'; 
+        $mail->Port       = 587;        
+
+        // Destinatarios del correo(remitente y receptor(dauphin.swim@gmail.com electromagnetico1ntegrado@gmail.com m1guel.sant1ag0@ciencias.unam.mx)) 
+        $mail->setFrom('miguel.perez@subitus.com', 'Miguel Subitus');
+        $mail->addAddress($emailTo);
+
+        //Contenido del mensaje
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $bodyEmail;
+
+        $mail->send();
+        echo 'El mensaje se envío correctamente';
+    }catch(Exception $e){
+        echo "Error al enviar mensaje: {$mail->ErrorInfo}";
+    }
+?>
